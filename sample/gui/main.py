79088fb38de9c87c914ee1cc1e2e61165188d70e
moiling <super8moi@gmail.com>
2019-05-19 23:56:44 +0800

    main window

--- sample/gui/main.py
+++ sample/gui/main.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-05-19 22:54
+# @Author  : moiling
+# @File    : main.py
 import sys
 
+from gui.ui.mainWindow import Ui_MainWindow
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import QtGui
-
-from exif import Exif
-from libPicmap.const import *
-from gui.ui.mainWindow import Ui_MainWindow
 
 
 class MainWindow(Ui_MainWindow, QMainWindow):
@@ -16,48 +18,6 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.setAcceptDrops(True)
-
-    def dragEnterEvent(self, evn):
-        evn.accept()
-
-    def dropEvent(self, evn: QtGui.QDropEvent):
-        file_url = evn.mimeData().text()
-        if file_url[:7] != 'file://':
-            # TODO 显示失败原因在界面上
-            return
-        if file_url[9] == ':':
-            # file:///C:,第9位是:的大概就是windows了
-            img_url = evn.mimeData().text()[8:]
-        else:
-            img_url = evn.mimeData().text()[7:]
-
-        # 显示图片
-        pix = QPixmap(img_url)
-        if pix.width() is not 0:
-            pix = pix.scaled(200, int(pix.height() / pix.width() * 200))
-
-        self.imgLabel.setPixmap(pix)
-
-        # 读Exif信息
-        e = Exif(img_url)
-
-        if not e.succeed:
-            # TODO 显示失败原因在界面上
-            return
-
-        # 显示所有Exif信息
-        self.textLabel.setText(e.__str__())
-
-        longitude, latitude = e.location()
-
-        if not e.succeed:
-            # TODO 显示失败原因在界面上
-            return
-
-        # 调用高德地图API，将地图网页显示在web engine view上
-        dest_name = "照片拍摄地点"
-        self.webEngineView.load(QUrl("https://m.amap.com/navi/?dest=%lf,%lf&destName=%s&hideRouteIcon=1&key=%s" %
-                                     (longitude, latitude, dest_name, api.url_api_key)))
 
 
 if __name__ == '__main__':
---
+++ sample/gui/ui/testExifWindow.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'testExifWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(602, 450)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 322, 400))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.webEngineView = QtWebEngineWidgets.QWebEngineView(self.scrollAreaWidgetContents)
+        self.webEngineView.setUrl(QtCore.QUrl("about:blank"))
+        self.webEngineView.setObjectName("webEngineView")
+        self.verticalLayout_2.addWidget(self.webEngineView)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.horizontalLayout.addWidget(self.scrollArea)
+        self.scrollArea_2 = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 260, 400))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents_2)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.imgLabel = QtWidgets.QLabel(self.scrollAreaWidgetContents_2)
+        self.imgLabel.setObjectName("imgLabel")
+        self.verticalLayout.addWidget(self.imgLabel)
+        self.textLabel = QtWidgets.QLabel(self.scrollAreaWidgetContents_2)
+        self.textLabel.setText("")
+        self.textLabel.setObjectName("textLabel")
+        self.verticalLayout.addWidget(self.textLabel)
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.horizontalLayout.addWidget(self.scrollArea_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 18))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionSave)
+        self.menuHelp.addAction(self.actionAbout)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.imgLabel.setText(_translate("MainWindow", "将照片拖至此处"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionOpen.setText(_translate("MainWindow", "Open"))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+
+from PyQt5 import QtWebEngineWidgets
